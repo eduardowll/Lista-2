f40049962531ebb86a5d22a683ef0ac1e1291e04
eduardowll <164221685+eduardowll@users.noreply.github.com>
2024-03-24 11:20:11 -0300

    questao2.cpp

---
+++ questao2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+/*
+Leia um número, verifique e escreva se este número é par ou ímpar. Se for par, verifique e escreva se é 
+maior que 100 ou não e se for ímpar verifique e escreva se é positivo ou negativo.
+*/
+
+int main()
+{
+    int numero;
+    
+    cout << "Digite um número inteiro: ";
+    cin >> numero;
+    
+    if(numero%2 == 0){
+    cout << "Seu número e par";
+        if(numero > 100){
+        cout << " e maior que 100";
+        }else{
+        cout << " e menor que 100";
+        }
+    }else{
+    cout << "Seu número é impar";
+        if(numero > 0){
+        cout << " e positivo";
+        }else{
+        cout << " e negativo";   
+        }
+    }
+}
